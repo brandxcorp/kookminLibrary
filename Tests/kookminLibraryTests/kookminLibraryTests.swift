edcc42a0a93b8f719930626f17b048b3aa1d37a7
brandx-taeil <taylor.son@brandxfitness.co.kr>
2023-07-17 17:41:34 +0900

    feat: Packages file

---
+++ Tests/kookminLibraryTests/kookminLibraryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import kookminLibrary
+
+final class kookminLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(kookminLibrary().text, "Hello, World!")
+    }
+}
